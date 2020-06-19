46dd34d90c136f88bddb134b0f9a63dd7c8bc82a
ravas <z.kinshef@gmail.com>
2020-06-19 09:48:38 +0300

    feat: 18.06.20

---
+++ vasili-0620-tehnosad-besedka2/js/data.js
@@ -0,0 +1,33 @@
+var calculator = {
+    "products": {
+        "astra": {
+            "2": 274,
+            "3": 354,
+            "4": 494
+        },
+        "astra-2": {
+            "2": 306,
+            "3": 403,
+            "4": 560
+        },
+        "pion": {
+            "2": 306,
+            "3": 403,
+            "4": 560
+        },
+        "tylpan": {
+            "2": 274,
+            "3": 354,
+            "4": 494
+        }
+    },
+    "additional": {
+        "fortochka": 27,
+        "poliv": 39,
+        "intelect": 27,
+        "parnik": 16,
+        "dostavka": 0,
+        "sborka": 0,
+        "rassrochka": 0
+    }
+};
