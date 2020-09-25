9a4c9aa5eb399c775b00dd67426543c010ebc1c8
ravas <z.kinshef@gmail.com>
2020-09-25 16:01:51 +0300

    feat: 25.09.20

---
+++ vasili-0920-tehnodacha-motobloki/public/scripts/data.js
@@ -0,0 +1,214 @@
+var calculatorData = {
+    "kult1000": {
+        'price': 728,
+        'img': './public/assets/img/catalog/1000.jpg',
+        'title': 'Культиватор Shtenli 1000 (6,0 лс)',
+        'parametrs': [
+            'Германия', // Производитель: _____
+            'Бензиновый, 4-х тактный', // Тип двигателя: ____________
+            'GX260', // Марка двигателя: ______
+            '8,5 л.с.', // Мощность: ______
+            '226 куб.см', // Объём двигателя: ____
+            '1', // Кол-во цилиндров: __
+            'Ручной', // Запуск двигателя: _______
+            'Воздушное', // Охлаждение: ______
+            '6 л.', // Ёмкость топливного бака: ______
+            'АИ-92, АИ-95', // Тип топлива: _____
+            '0,9 л/ч', // Расход топлива: _____
+            '6,5х12', // Размер колёс: _____
+            'Да', // Дифференциал: ___
+            'Нет', // ВОМ: ____
+            '3-1', // Кол-во передач: ____
+            'Да', // Пониженная передача: ____
+            '30 см.', // Глубина культивации: _____
+            '120 см.', // Ширина культивации: _____
+            '399 об/мин.', // Скорость вращения фрез: _____
+            'Дисковое', // Сцепление: _______
+            'Шестерёнчатый', // Редуктор: ______
+            '120 кг.', // Вес: _____
+        ]
+    },
+    "kult7000": {
+        'price': 766,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/7000.jpg',
+        'title': 'Культиватор Shtenli 7000 EXPERT (6,0 лс)',
+    },
+    "kult8000": {
+        'price': 894,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/8000.jpg',
+        'title': 'Культиватор Shtenli 8000 EXPERT (7,0 лс)',
+    },
+    "kult500": {
+        'price': 997,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/500.jpg',
+        'title': 'Культиватор Shtenli 500 (7,0 лс)',
+    },
+    "kult900": {
+        'price': 1023,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/900.jpg',
+        'title': 'Культиватор Shtenli 900 (8,0 лс)',
+    },
+    "kult1030nl": {
+        'price': 1068,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1030nl.jpg',
+        'title': 'Культиватор Shtenli 1030 NL (8,5 лс)',
+    },
+    "kult1030l": {
+        'price': 1158,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Культиватор Shtenli 1030 L (8,5 лс)',
+    },
+    "kult1030bezVOM": {
+        'price': 1202,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Культиватор Shtenli 1030 без ВОМ (8,5 лс)',
+    },
+    "kult1030sVom": {
+        'price': 1411,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Мотоблок Shtenli 1030 с ВОМ (8,5 лс)',
+    },
+    "kult1030PL": {
+        'price': 1305,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Культиватор Shtenli 1030 PL (8,5 лс)',
+    },
+    "kult1030P": {
+        'price': 1390,
+        'priceold': 389,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Культиватор Shtenli 1030 P (8,5 лс)',
+    },
+    "motoblok1030PsVOM": {
+        'price': 1489,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/kult1030.jpg',
+        'title': 'Мотоблок Shtenli 1030 P с ВОМ (8,5 лс)',
+    },
+    "1900_14-PL": {
+        'price': 1489,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Мотоблок Shtenli 1900 (14-PL)',
+    },
+    "1900_14-P": {
+        'price': 1800,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Культиватор Shtenli 1900 (14-P) без ВОМ',
+    },
+    "1900_14-P_sVOM": {
+        'price': 1866,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (14-P) с ВОМ',
+    },
+    "1900_14-RK1": {
+        'price': 1953,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900PRO.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (14-РК1) с ВОМ',
+    },
+    "1900_16_P": {
+        'price': 1866,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Культиватор Shtenli 1900 (16-Р) без ВОМ',
+    },
+    "1900_16_P_vom": {
+        'price': 1946,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (16-P) с ВОМ',
+    },
+    "1900_16-PК1_vom": {
+        'price': 2033,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900PRO.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (16-PК1) с ВОМ',
+    },
+    "1900_18-P": {
+        'price': 1932,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Культиватор Shtenli 1900 (18-P) без ВОМ',
+    },
+    "1900_18-P_vom": {
+        'price': 2026,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (18-P) с ВОМ',
+    },
+    "1900_18-PК1_vom": {
+        'price': 2113,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1900PRO.jpg',
+        'title': 'Мотоблок Shtenli 1900 PRO (18-PК1) с ВОМ',
+    },
+    "1030_8-5": {
+        'price': 1932,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/1030AD.jpg',
+        'title': 'Мотоблок Shtenli 1030 P с адаптером (8,5 л.с)',
+    },
+    "1900_14-PК1": {
+        'price': 3680,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/190014.jpg',
+        'title': 'Минитрактор Shtenli 1900А PRO (14-PК1)',
+    },
+    "G-180": {
+        'price': 2829,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/G.jpg',
+        'title': 'Мотоблок Shtenli G-180 (8,0 лс)',
+    },
+    "G-185": {
+        'price': 2929,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/G.jpg',
+        'title': 'Мотоблок Shtenli G-185 (10,5 лс)',
+    },
+    "G-192": {
+        'price': 3604,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/G.jpg',
+        'title': 'Мотоблок Shtenli G-192 (12 лс)',
+    },
+    "480": {
+        'price': 591,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/PR.jpg',
+        'title': 'Прицеп МП-480, ТМ-1300',
+    },
+    "600": {
+        'price': 692,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/PR.jpg',
+        'title': 'Прицеп МП-600 (самосвальный), ТМ-1700 (самосвальный)',
+    },
+    "700": {
+        'price': 826,
+        'priceold': 1041,
+        'img': './public/assets/img/catalog/PR.jpg',
+        'title': 'Прицеп МП-700 (самосвальный), ТМ-1850 (самосвальный)',
+    },
+    // "navesnProduct1": {
+    //     'price': 1620,
+    //     'img': './img/catalog/profi4.jpg',
+    //     'title': 'Большой плуг',
+    // },
+    // "navesnProduct2": {
+    //     'price': 820,
+    //     'img': './img/catalog/Plug.jpg',
+    //     'title': 'Большой плуг2 eeeeeee eee',
+    // },
+}
---
+++ vasili-0920-tehnodacha-motobloki/src/scripts/js.js
@@ -0,0 +1,222 @@
+$(function () {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        if(typeof sessionStorage !== 'undefined'){
+            if(sessionStorage.getItem('formSubmitted')){
+                if(!confirm('Вы уже отправили заявку, повторить?')){return false}
+            }else{
+                sessionStorage.setItem('formSubmitted', 'true')
+            }
+        }
+        var data = $(this).serializeArray();
+        data.push({
+            name: "source",
+            value: "Test"
+        });
+        data.push({
+            name: "title",
+            value: "Test message"
+        });
+        data.push({
+            name: "link",
+            value: location.href
+        });
+
+        console.log(JSON.stringify(data));
+        return false; // Testing
+
+        $.ajax({
+            type: "POST",
+            url: "https://skidka-tut.by/action/index.php",
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            dataType: "json",
+            data: data,
+        }).done(function (response) {
+            alert(response.text);
+        }).fail(function (error, textStatus) {
+            console.log(error, textStatus);
+            alert('Извините, произошла ошибка запроса. Свяжитесь с менеджером по телефону!');
+        });
+
+        // Event dispatcher for IE9+ included
+        if(typeof(Event) === 'function') {
+            document.dispatchEvent(new Event('app.form.send'));
+        }else{
+            var ev = document.createEvent('Event');
+            ev.initEvent('app.form.send', false, false);
+            document.dispatchEvent(ev);
+        }
+
+        //console.log(JSON.stringify(data))
+        return false
+    });
+
+    $("a.smoothscroll").click(function(a) {
+        "" !== this.hash && (a.preventDefault(), a = this.hash, $("html, body").animate({
+            scrollTop: $(a).offset().top - 100
+        }, 400))
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    try {
+        var mainNav = document.querySelector('.btn-top');
+        document.onscroll = function () {
+            if(window.pageYOffset >= 400){
+                mainNav.classList.add('active');
+            } else {
+                mainNav.classList.remove('active');
+            }
+        };
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        var localNevValue = function(key, nevValue, obgect){
+            var inquiry = JSON.parse(localStorage.getItem(key));
+            if(obgect){
+                var Value = {...inquiry, [obgect]: {...inquiry[obgect], ...nevValue}}
+            }else{
+                var Value = {...inquiry, ...nevValue}
+            }
+            localStorage.setItem(key, JSON.stringify(Value));
+        }
+    
+        var localDeleteValue = function(key, deleteObgect){
+            var json = {...oldJson};
+            delete json[deleteObgect];
+            localStorage.setItem(key, JSON.stringify(json));
+        }
+    
+        var eventHandler = function(){
+            Array.prototype.forEach.call(document.querySelectorAll('.product-item'), function (item){
+                var itemProduct = item.querySelector('input[name="product"]').value;
+                item.querySelector('.product-item__close').onclick = function (){
+                    localDeleteValue('tcart', itemProduct)
+                    displayResult()
+                }
+                item.querySelector('.product-item__minus').onclick = function (){
+                    var nevValCol = --oldJson[itemProduct].col
+                    var nevValPrice = oldJson[itemProduct].onePrice * nevValCol;
+                    if(nevValCol > 0){
+                        localNevValue('tcart', {col: nevValCol, price: nevValPrice}, itemProduct)
+                        displayResult()
+                    }else{
+                        localDeleteValue('tcart', itemProduct)
+                        displayResult()
+                    }
+                }
+                item.querySelector('.product-item__plus').onclick = function (){
+                    var nevValCol = ++oldJson[itemProduct].col
+                    var nevValPrice = oldJson[itemProduct].onePrice * nevValCol;
+                    localNevValue('tcart', {col: nevValCol, price: nevValPrice}, itemProduct)
+                    displayResult()
+                }
+    
+                item.querySelector('.product-item__col-span').onclick = function (){
+                    this.style.display = 'none';
+                    item.querySelector('.product-item__col-input').value = oldJson[itemProduct].col;
+                    item.querySelector('.product-item__col-input').style.display = 'block';
+                    item.querySelector('.product-item__col-input').focus()
+                }
+                item.querySelector('.product-item__col-input').onblur = function (){
+                    this.style.display = 'none';
+                    if(this.value>0){
+                        var nevValPrice = oldJson[itemProduct].onePrice * this.value;
+                        localNevValue('tcart', {col: +this.value, price: nevValPrice}, itemProduct)
+                    }else{
+                        localDeleteValue('tcart', itemProduct)
+                    }
+                    displayResult()
+                    item.querySelector('.product-item__col-span').style.display = 'block';
+                }
+            })
+        }
+    
+        var oldJson = JSON.parse(localStorage.getItem('tcart'));
+        if(oldJson === null){localStorage.setItem('tcart', JSON.stringify({}));}
+        var displayResult = function(){
+            document.querySelector('.modal-product__total-price').innerHTML = 0;
+            oldJson = JSON.parse(localStorage.getItem('tcart'));
+            document.querySelector('.product-wrap').innerHTML = '';
+            var itogCatalog = {itogPrice: 0, itogCol: 0};
+            for (var key in oldJson){
+                itogCatalog.itogPrice = itogCatalog.itogPrice + oldJson[key].price;
+                itogCatalog.itogCol = itogCatalog.itogCol + oldJson[key].col;
+                document.querySelector('.product-wrap').innerHTML += '<div class="product-item my-2 font-weight-light"><input type="hidden" name="product" value="'+key+'"><input type="hidden" name="productName-col" value="'+oldJson[key].title+'-'+oldJson[key].col+'"><div class="row align-items-center text-center"><div class="col-3"><img src="'+oldJson[key].img+'"></div><div class="col-3 p-0 text-left"><div class="form-item__title">'+oldJson[key].title+'</div></div><div class="col-3 d-flex justify-content-around"><span class="product-item__minus"><span class="fa-stack"><i class="fa fa-circle-thin fa-stack-2x"></i><i class="fa fa-minus fa-stack-1x"></i></span></span><span class="product-item__col-span">'+oldJson[key].col+'</span><input class="product-item__col-input text-center" style="display: none;" type="number" name="product-item__col" required /><span class="product-item__plus"><span class="fa-stack"><i class="fa fa-circle-thin fa-stack-2x"></i><i class="fa fa-plus fa-stack-1x"></i></span></span></div><div class="col-3 d-flex justify-content-around"><span class="product-item__price">'+oldJson[key].price+' р.</span><span class="product-item__close"><span class="fa-stack"><i class="fa fa-circle-thin fa-stack-2x"></i><span class="fa-stack-1x">&#10005;</span></span></span></div></div></div>';
+            }
+            if(Object.keys(oldJson).length){
+                localNevValue('itogCatalog', itogCatalog)
+                document.querySelector('.active-catalog').style.display = 'block';
+                document.querySelector('.active-catalog__item').textContent = JSON.parse(localStorage.getItem('itogCatalog')).itogCol;
+                document.querySelector('.active-catalog__col').textContent = JSON.parse(localStorage.getItem('itogCatalog')).itogPrice + ' р.';
+                document.querySelector('.modal-product__total-price').textContent = JSON.parse(localStorage.getItem('itogCatalog')).itogPrice;
+                eventHandler()
+            }else {
+                localStorage.removeItem('itogCatalog');
+                document.querySelector('.active-catalog').style.display = 'none';
+                $('.modal').modal('hide');
+            }
+        }
+        displayResult()
+    
+    
+        Array.prototype.forEach.call(document.querySelectorAll('.has-calculator, .modal-primer'), function (item) {
+            var itemProduct = item.querySelector('input[name="product"]').value;
+            
+            if(item.classList.contains("modal-primer")){
+                item.id = 'modal-'+item.querySelector('input[name="product"]').value+'-primer';
+            }
+
+            if(item.querySelector('.has-calculator__img').href){
+                item.querySelector('.has-calculator__img').dataset.fancybox = itemProduct;
+                item.querySelector('.has-calculator__img').href = calculatorData[itemProduct].img;
+            }
+            item.querySelector('.has-calculator__img img').src = calculatorData[itemProduct].img;
+            if(item.querySelector('.has-calculator__btnDetails')){
+                item.querySelector('.has-calculator__btnDetails').href = '#modal-'+ itemProduct +'-primer';
+            }
+            item.querySelector('.has-calculator__title').textContent = calculatorData[itemProduct].title;
+            item.querySelector('.has-calculator__price').innerHTML = '<div class="h3 text-red d-inline-block"><span class="jPrice">'+calculatorData[itemProduct].price+'</span> руб.</div>';
+            if(calculatorData[itemProduct].priceOld){
+                item.querySelector('.has-calculator__price').innerHTML += '<s class="h5 text-muted font-weight-light d-inline-block mx-2"><span class="jPriceOld">'+calculatorData[itemProduct].price+'</span> руб.</s>';
+            }
+            item.onclick = function () {
+                if(event.target && event.target.getAttribute('href') === ('#modal-product')) {
+                    $('.modal').modal('hide');
+                    if(oldJson.hasOwnProperty(itemProduct)){
+                        var nevValCol = ++oldJson[itemProduct].col;
+                        var nevValPrice = oldJson[itemProduct].onePrice * nevValCol;
+                        localNevValue('tcart', {col: nevValCol, price: nevValPrice}, itemProduct)
+                        displayResult()
+                    }else {
+                        var nevVal = {col:1, onePrice: calculatorData[itemProduct].price}
+                        var json = {...oldJson, [itemProduct]: { ...calculatorData[itemProduct], ...nevVal} }
+                        localStorage.setItem('tcart', JSON.stringify(json));
+                        displayResult()
+                    }
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
