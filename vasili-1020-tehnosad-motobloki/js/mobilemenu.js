e1b040e8f84ecb87c897e2a1cb82979ad4f43737
ravas <z.kinsheg@gmaol.com>
2020-10-22 16:35:47 +0300

    ready dor vasili-1020-tehnosad-motobloki

--- vasili-1020-tehnosad-motobloki/js/mobilemenu.js
+++
@@ -1,6 +0,0 @@
-$("#navToggle").click(function() {
-    $(this).toggleClass("active");
-    $(".overlay").toggleClass("open");
-    // this line ▼ prevents content scroll-behind
-    $("body").toggleClass("locked"); 
-});
