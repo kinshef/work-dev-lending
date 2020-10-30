c3589fbfcd56fe69f9fbedb8a68ae57a55c0957c
ravas <z.kinshef@gmail.com>
2020-10-30 16:30:50 +0300

    start dor vasili-1020-agrosila-moto

---
+++ vasili-1020-agrosila-moto/src/scripts/js.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        if(typeof sessionStorage !== 'undefined'){
+            if(sessionStorage.getItem('formSubmitted')){
+                if(!confirm('Вы уже отправили заявку, повторить?')){return false}
+            }else{
+                sessionStorage.setItem('formSubmitted', 'true')
+            }
+        }
+        var data = $(this).serializeArray();
+        data.push({
+            name: "source",
+            value: "Test"
+        });
+        data.push({
+            name: "title",
+            value: "Test message"
+        });
+        data.push({
+            name: "link",
+            value: location.href
+        });
+
+        /*console.log(JSON.stringify(data));
+        return false;*/ // Testing
+
+        $.ajax({
+            type: "POST",
+            url: "https://skidka-tut.by/action/index.php",
+            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            dataType: "json",
+            data: data,
+        }).done(function (response) {
+            alert(response.text);
+        }).fail(function (error, textStatus) {
+            console.log(error, textStatus);
+            alert('Извините, произошла ошибка запроса. Свяжитесь с менеджером по телефону!');
+        });
+
+        // Event dispatcher for IE9+ included
+        if(typeof(Event) === 'function') {
+            document.dispatchEvent(new Event('app.form.send'));
+        }else{
+            var ev = document.createEvent('Event');
+            ev.initEvent('app.form.send', false, false);
+            document.dispatchEvent(ev);
+        }
+
+        //console.log(JSON.stringify(data))
+        return false
+    });
+
+    $("a.smoothscroll").click(function(a) {
+        "" !== this.hash && (a.preventDefault(), a = this.hash, $("html, body").animate({
+            scrollTop: $(a).offset().top
+        }, 400))
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
