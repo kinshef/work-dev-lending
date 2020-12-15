215dc97da6a28eef1adb2c44ba665aa4417746b5
ravas <z.kinshef@gmail.com>
2020-12-15 16:54:56 +0300

    rafactor vasili-1220-torguem-svarka

--- vasili-1220-torguem-svarka/src/scripts/js.js
+++ vasili-1220-torguem-svarka/src/scripts/js.js
@@ -1,4 +1,137 @@
-$(function(){$.noty.defaults.theme="defaultTheme";$.noty.defaults.layout="top";$("form").submit(function(a){a.preventDefault();if("undefined"!==typeof sessionStorage)if(sessionStorage.getItem("formSubmitted")){if(!confirm("\u0412\u044b \u0443\u0436\u0435 \u043e\u0442\u043f\u0440\u0430\u0432\u0438\u043b\u0438 \u0437\u0430\u044f\u0432\u043a\u0443, \u043f\u043e\u0432\u0442\u043e\u0440\u0438\u0442\u044c?"))return!1}else sessionStorage.setItem("formSubmitted","true");a=$(this).serializeArray();a.push({name:"subject",
-    value:"\u0411\u0435\u0441\u0435\u0434\u043a\u0430"});a.push({name:"action",value:"sendMail"});a.push({name:"toEmail",value:"order"});a.push({name:"link",value:location.href});$.ajax({type:"POST",url:"/system-page/ajax.html",dataType:"html",data:a,beforeSend:function(){window.nSend=noty({text:"\u041e\u0442\u043f\u0440\u0430\u0432\u043a\u0430 \u0437\u0430\u043f\u0440\u043e\u0441\u0430...",type:"information",animation:{open:!1,close:!1}})}}).done(function(a){noty({text:a,type:"success",timeout:1E4})}).fail(function(a, c){console.log(a,c);noty({text:"\u0418\u0437\u0432\u0438\u043d\u0438\u0442\u0435, \u043f\u0440\u043e\u0438\u0437\u043e\u0448\u043b\u0430 \u043e\u0448\u0438\u0431\u043a\u0430 \u0437\u0430\u043f\u0440\u043e\u0441\u0430. \u0421\u0432\u044f\u0436\u0438\u0442\u0435\u0441\u044c \u0441 \u043c\u0435\u043d\u0435\u0434\u0436\u0435\u0440\u043e\u043c \u043f\u043e \u0442\u0435\u043b\u0435\u0444\u043e\u043d\u0443!",type:"error"})}).always(function(){window.nSend.close()});try{ym(47791231,"reachGoal","form_send")}catch(b){console.log(b)}return!1});
-    $('[data-toggle="popover"]').popover();$("a.smoothscroll").click(function(a){""!==this.hash&&(a.preventDefault(),a=this.hash,$("html, body").animate({scrollTop:$(a).offset().top},400))});$.fn.nav=function(a){var b={offset:0};$.extend(b,a);var c=this;$(c).each(function(a,e){var d=$(e.hash);if(d.length){var f=$(d).offset();$(window).scroll(function(){var a=$(window).scrollTop()+b.offset;f.top<a&&a<f.top+$(d).height()&&($(c).removeClass("active"),$(e).addClass("active"))})}})};$("nav a.js-nav-link").nav({offset:150})});
-document.addEventListener("DOMContentLoaded",function(){var a=new Date;a.setDate(a.getDay()?a.getDate()-a.getDay()+8:a.getDate()+1);a.setHours(0,0,0);new LightCountdown(".countdown-week",a)});var app={h:"cHJvbW8uYWdyb2NhdGFsb2cuYnk="};
+$(function () {
+    // Notification plugin global settings
+    $.noty.defaults.theme = 'defaultTheme' //defaultTheme, metroui, relax
+    $.noty.defaults.layout = 'top'
+    
+    // Form submit
+    $("form").submit(function (event) {
+        event.preventDefault()
+
+        if(typeof sessionStorage !== 'undefined'){
+            if(sessionStorage.getItem('formSubmitted')){
+                if(!confirm('Вы уже отправили заявку, повторить?')){return false}
+            }else{
+                sessionStorage.setItem('formSubmitted', 'true')
+            }
+        }
+
+        var data = $(this).serializeArray()
+        data.push({
+            name: "subject",
+            value: "Сварка"
+        })
+        data.push({
+            name: "action",
+            value: "sendMail"
+        })
+        data.push({
+            name: "toEmail",
+            value: "order"
+        })
+        data.push({
+            name: "link",
+            value: location.href
+        })
+
+        /*console.log(JSON.stringify(data));
+        return false;*/
+
+        $.ajax({
+            type: "POST",
+            url: "/system-page/ajax.html", // http://skidka-tut.by/mail/ajax.php
+            dataType: "html",
+            data: data,
+            beforeSend: function () {
+                window.nSend = noty({
+                    text: 'Отправка запроса...',
+                    type: 'information',
+                    animation: {
+                        open: false,
+                        close: false
+                    }
+                });
+            }
+        }).done(function (response) {
+            noty({
+                text: response,
+                type: 'success',
+                timeout: 10000
+            })
+        }).fail(function (error, textStatus) {
+            console.log(error, textStatus)
+            noty({
+                text: 'Извините, произошла ошибка запроса. Свяжитесь с менеджером по телефону!',
+                type: 'error'
+            })
+        }).always(function () {
+            window.nSend.close()
+        })
+
+        try {
+            ym(47791231, 'reachGoal', 'form_send');
+        }catch (error) {
+            console.log(error)
+        }
+
+        //console.log(JSON.stringify(data))
+        return false
+    })
+    $('[data-toggle="popover"]').popover()
+    
+    // Smooth scroll
+    $("a.smoothscroll").click(function (event) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 400);
+        } // End if
+    })
+    
+    $.fn.nav = function (o) {
+        var options = {
+            offset: 0
+        }
+        $.extend(options, o)
+        var match = this
+        $(match).each(function (i, e) {
+            var div = $(e.hash)
+            if (div.length) {
+                var divOffset = $(div).offset()
+                $(window).scroll(function () {
+                    var scrolled = $(window).scrollTop() + options.offset
+                    if (divOffset.top < scrolled && scrolled < divOffset.top + $(div).height()) {
+                        $(match).removeClass('active')
+                        $(e).addClass('active')
+                    }
+                })
+            }
+        })
+    }
+
+    $('nav a.js-nav-link').nav({
+        offset: 150
+    })
+    
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    /* countdown */
+    var dateEnd = new Date();
+    dateEnd.setDate(dateEnd.getDay() ? dateEnd.getDate() - dateEnd.getDay() + 8 : dateEnd.getDate() + 1);
+    dateEnd.setHours(0, 0, 0);
+    var countdown = new LightCountdown('.countdown-week', dateEnd);
+
+});
+
+// SETTINGS
+var app = {
+    h: 'cHJvbW8uYWdyb2NhdGFsb2cuYnk='
+}
